a059607af775b74abb18498e4f9b03eb3ac35199
Daniel_Malaver <malaverdaniel22@gmail.com>
2021-07-12 12:02:31 -0400

    feat: requirements.txt refactor: energy_load

--- src/sqlite_manager.py
+++ src/sqlite_manager.py
@@ -10,13 +10,10 @@ import json
 def energy_load(loras):
     conn = sqlite3.connect('meter_db.sqlite')
     cur = conn.cursor()
-    cur.execute('DROP TABLE IF EXISTS meter_table ')#El objetivo de la base de dato es almacenar no se puede borrar
-    #cur.execute("CREATE TABLE meter_table (meter_id STRING, energy INTEGER, date STRING, state BOOLEAN)")  #Original
-    cur.execute("CREATE TABLE meter_table (meter_id STRING, energy INTEGER, date TEXT, state BOOLEAN)")              #Para el test
+    #cur.execute('DROP TABLE IF EXISTS meter_table ')#El objetivo de la base de dato es almacenar no se puede borrar
+    #cur.execute("CREATE TABLE meter_table (meter_id STRING, energy INTEGER, date TEXT, state BOOLEAN)")    #Para el test
     conn.commit()
-    nuevo = 0 # unused un mejor nombre pudiese ser isInDataBase por ser boolean
     #lsb en nombres mas human readable
-    #
     for lora in loras:# Prueba de esta manera
         
         msb_4 = (lora["loraid"]).to_bytes(2,'big')
@@ -24,32 +21,29 @@ def energy_load(loras):
         for slave in lora['slaves']:
             lsb_2 = (slave).to_bytes(1,'big')
             meter_id = msb_4+lsb_2
-            print(meter_id.hex()) # Mira como ya se imprime
+            #print(meter_id.hex()) # Mira como ya se imprime
             cur.execute('SELECT * FROM meter_table WHERE meter_id = ?',(meter_id.hex(), ))
             if cur.fetchone() == None:
                 date = datetime.datetime.now()
-                cur.execute("INSERT INTO meter_table(meter_id,energy,date,state) VALUES(?,?,?,?)",(meter_id.hex(),0,date,1))              #Modificado para el test
+                cur.execute("INSERT INTO meter_table(meter_id,energy,date,state) VALUES(?,?,?,?)",(meter_id.hex(),0,date,1))      #Modificado para el test
             conn.commit() 
     cur.close()
 
 
-def load_json(id, write_api_key):# Usa los datos de entrada
+def load_json(id, write_api_key):
     conn = sqlite3.connect('meter_db.sqlite')
     cur = conn.cursor()
-    data = {"id": "0001","write_api_key": "PYF7YMZNOM3TJVSM",}
+    data = {"id": id,"write_api_key": write_api_key,}
     data['updates'] = []
     cur.execute('SELECT * FROM meter_table')
     for row in cur:
         data['updates'].append({
             "meterid": row[0],
             "energy": row[1],
-            #"date": row[2],            #Original
-            "date": 0,                  #Para el test
+            "date": row[2],            #Original
+            #"date": 0,                  #Para el test
             "state": row[3]
             })
-    with open('data.json', 'w') as file: # NO hay necesidad de escribir un archivo
-                                            #con el dicccionario es sufieciente
-        json.dump(data, file, indent=4)
     cur.close()
     return data
                 
@@ -60,7 +54,7 @@ def update_date_base(meterid, data):
 if __name__ == '__main__':
     print("Hello world")
     loras = [{"loraid":255,"slaves":[1,2,3,4]},{"loraid":254,"slaves":[0]}]
-    #energy_load(loras)
+    energy_load(loras)
     return_json =load_json(id= "0001", write_api_key="PYF7YMZNOM3TJVSM")
     print(return_json)
     
